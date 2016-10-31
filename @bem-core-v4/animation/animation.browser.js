5e53c9a2de1a1c7dc1d9446ee7e94f238bea955f
Sergey Belozyorcev <nextdrift@gmail.com>
2016-10-31 23:49:55 +0300

    Add animation events: start, end, iteration

--- @bem-core-v4/animation/animation.browser.js
+++ @bem-core-v4/animation/animation.browser.js
@@ -1,3 +1,61 @@
+/**
+ * @module animation
+ */
+
 modules.define('animation', ['i-bem-dom'], function(provide, bemDom) {
-  provide(bemDom.declBlock(this.name));
+  /**
+   * @exports
+   * @class animation
+   * @bem
+   */
+  provide(bemDom.declBlock(this.name, /** @lends animation.prototype */ {
+    /**
+     * Animation Start Event
+     */
+    _onStart: function() {
+      this._emit('start');
+    },
+
+    /**
+    * Animation End Event
+    */
+    _onEnd: function() {
+      this._emit('end');
+    },
+
+    /**
+    * Animation Iteration Event
+    */
+    _onIteration: function() {
+      this._emit('iteration');
+    }
+  }, /** @lends animation */ {
+    lazyInit: true,
+    onInit: function() {
+      let animationEnd = [
+        'animationend',
+        'oanimationend',
+        'webkitAnimationEnd',
+        'MSAnimationEnd'
+      ];
+      let animationStart = [
+        'animationstart',
+        'oanimationstart',
+        'webkitAnimationStart',
+        'MSAnimationStart'
+      ];
+      let animationIteration = [
+        'animationiteration',
+        'oanimationiteration',
+        'webkitAnimationIteration',
+        'MSAnimationIteration'
+      ];
+
+      this._domEvents()
+        .on(animationEnd.join(' '), this.prototype._onEnd)
+        .on(animationStart.join(' '), this.prototype._onStart)
+        .on(animationIteration.join(' '), this.prototype._onIteration);
+      return this.__base.apply(this, arguments);
+    }
+  }));
 });
