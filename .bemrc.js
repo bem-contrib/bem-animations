069f798a5ab53e5cb44af6135f71dfad222677a4
Sergey Belozyorcev <nextdrift@gmail.com>
2018-03-10 04:11:04 +0300

    Add bemrc

---
+++ .bemrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    root : true,
+    levels : [
+        {
+            layer : 'common',
+            path : '@common'
+        }
+    ],
+    sets : {
+        common : 'common'
+    }
+};
